7e077b7d152e5b8aa7bcb33cff831697405c2b8a
Jesus Magana <maganazero5@gmail.com>
2014-08-08 20:45:26 -0700

    Implemented a class

---
+++ js/person.js
@@ -0,0 +1,12 @@
+var app = angular.module("myApp", [])
+
+app.controller('ParentController', function($scope){
+	$scope.person = {greeted: false};
+});
+
+app.controller('ChildController', function($scope){
+	$scope.sayHello = function(){
+		$scope.person.name = "Ari Lerner";
+		$scope.person.greeted = true;
+	}
+});
