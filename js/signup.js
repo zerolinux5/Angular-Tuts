174252c4504fb156830b4845c8edacf52274871c
Jesus Magana <maganazero5@gmail.com>
2014-08-19 13:29:37 -0700

    Fixed typos

---
+++ js/signup.js
@@ -0,0 +1,20 @@
+angular.module("myApp", [])
+.directive('ensureUnique', ['$http', function($http) {
+	return {
+		require: 'ngModel',
+		link: function(scope, ele, attrs, c) {
+			scope.$watch(attrs.ngModel, function(n) {
+				if (!n) return;
+				$http({
+					method: 'POST',
+					url: '/api/check/'+attrs.ensureUnique,
+					data: {'field': attrs.ensureUnique}
+				}).success(function(data) {
+					c.$setValidity('unique', data.isUnique);
+				}).error(function(data) {
+					c.$setValidity('unique', false);
+				});
+			});
+		}
+	};
+}]);
