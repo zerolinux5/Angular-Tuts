187d0e9cc52f66ed5f2650f0b7660523115df118
Jesus Magana <maganazero5@gmail.com>
2014-08-20 13:10:00 -0700

    Added some stylizing

--- simpleApp/js/main.js
+++ simpleApp/js/main.js
@@ -16,6 +16,20 @@ app.controller('HomeController', function() {
 
 });
 
+app.controller('MailListingController', function($scope) {
+	$scope.email = [{
+		'id': 1,
+		'from': 'fred@fullstack.io',
+		'to': 'ari@fullstack.io',
+		'subject': 'Great job',
+		'body': 'Congrats on the release of the book!'
+	}];
+});
+
+app.controller('ContentController', function($scope) {
+
+});
+
 app.controller('SettingsController', function($scope) {
 	$scope.settings = {
 		name: "Ari",
